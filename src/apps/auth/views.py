0f877d4ce6388ab74cb6aa9fcd929a53ee9e4a62
Pavel Rabekin <pavelrabekin@gmail.com>
2023-06-12 00:12:36 +0300

    added login form

--- src/apps/auth/views.py
+++ src/apps/auth/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.core.handlers.wsgi import WSGIRequest
 
+from src.apps.auth.forms import LoginForm
+
 
 def login(request: WSGIRequest):
-    print("index.html")
-    return render(request, "index.html")
+    form = LoginForm()
+    data = {
+        "form": form,
+    }
+    return render(request, "login.html", data)
