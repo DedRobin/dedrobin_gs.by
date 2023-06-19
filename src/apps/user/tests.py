8fc0c7464bb51ce0bd632190903d7a706444662d
Pavel Rabekin <pavelrabekin@gmail.com>
2023-06-19 20:56:40 +0300

    added register, login, logout tests

--- src/apps/user/tests.py
+++ src/apps/user/tests.py
@@ -8,7 +8,7 @@ from src.apps.user.factories import UserFactory
 from src.apps.user.models import CustomUser
 
 
-class LoginTests(LiveServerTestCase):
+class AuthTests(LiveServerTestCase):
     # fixtures = ["user-data.json"]
 
     @classmethod
@@ -27,10 +27,41 @@ class LoginTests(LiveServerTestCase):
         cls.browser.quit()
         super().tearDownClass()
 
-    def test_login(self):
+    def test_registration(self):
+        username = self.fake.user_name()
+        email = self.fake.email()
+        password = self.fake.md5()
+
+        self.browser.get(f"{self.live_server_url}")
+        register_xpath = '//*[@id="navbarSupportedContent"]/ul[2]/li[2]/a'
+        register_href = self.browser.find_element(By.XPATH, register_xpath).get_attribute("href")
+        self.browser.get(register_href)
+        assert self.browser.title == "Registration"
+
+        username_input = self.browser.find_element(By.NAME, "username")
+        username_input.send_keys(username)
+        email_input = self.browser.find_element(By.NAME, "email")
+        email_input.send_keys(email)
+        password1_input = self.browser.find_element(By.NAME, "password1")
+        password1_input.send_keys(password)
+        password2_input = self.browser.find_element(By.NAME, "password2")
+        password2_input.send_keys(password)
+        self.browser.find_element(By.CLASS_NAME, "btn-primary").click()
+        assert self.browser.title == "Login"
+
+    def test_login_and_logout(self):
+        email = self.user.email
+        password = self.user_password
+
         self.browser.get(f"{self.live_server_url}")
         username_input = self.browser.find_element(By.NAME, "email")
-        username_input.send_keys(self.user.email)
+        username_input.send_keys(email)
         password_input = self.browser.find_element(By.NAME, "password")
-        password_input.send_keys(self.user_password)
+        password_input.send_keys(password)
         self.browser.find_element(By.CLASS_NAME, "btn-primary").click()
+        assert self.browser.title == "DedRobin Game Service"
+
+        xpath = '/html/body/nav/div/div/ul[2]/li/ul/li[3]/a'
+        href_logout = self.browser.find_element(By.XPATH, xpath).get_attribute("href")
+        self.browser.get(href_logout)
+        assert self.browser.title == "Login"
