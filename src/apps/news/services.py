138f02f8cabadbe0e23b25c5cf0c5df4d5f32c4e
Pavel Rabekin <pavelrabekin@gmail.com>
2023-06-26 12:23:56 +0300

    added search input field

--- src/apps/news/services.py
+++ src/apps/news/services.py
@@ -38,7 +38,10 @@ def run_parser(clear):
 
 def get_page_from_request(request: WSGIRequest, queryset: QuerySet, obj_per_page: int) -> Page:
     paginator = Paginator(object_list=queryset, per_page=obj_per_page)
-    page_number = request.GET.get("page", 1)
+    if request.GET.get("follow_page"):
+        page_number = request.GET.get("follow_page")
+    else:
+        page_number = request.GET.get("page", 1)
     page_obj = paginator.get_page(page_number)
     return page_obj
 
