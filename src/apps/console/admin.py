4563e42eeebab91ebe9fb2eda479c574ae84a145
Pavel Rabekin <pavelrabekin@gmail.com>
2023-07-07 08:52:06 +0300

    added field 'image' for 'console' app, install Pillow

--- src/apps/console/admin.py
+++ src/apps/console/admin.py
@@ -6,4 +6,4 @@ from src.apps.console.models import Console
 @admin.register(Console)
 class ConsoleAdmin(admin.ModelAdmin):
     list_display = ("name", "price_per_hour", "price_per_month")
-    fields = ("name", "price_per_hour", "price_per_month")
+    fields = ("name", "price_per_hour", "price_per_month", "image")
--- src/apps/console/models.py
+++ src/apps/console/models.py
@@ -5,6 +5,7 @@ class Console(models.Model):
     name = models.CharField(max_length=150)
     price_per_hour = models.DecimalField(max_digits=5, decimal_places=2)
     price_per_month = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to="src/apps/console/static/images/console", blank=True, null=True)
 
     def __str__(self):
         return self.name
