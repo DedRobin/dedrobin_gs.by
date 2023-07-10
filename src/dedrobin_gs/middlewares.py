e9dc3fefdf2893851375a896a3691e455c4e8ecf
Pavel Rabekin <pavelrabekin@gmail.com>
2023-07-10 16:16:22 +0300

    added comment to middleware

--- src/dedrobin_gs/middlewares.py
+++ src/dedrobin_gs/middlewares.py
@@ -1,15 +1,10 @@
 class OrderCountMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-        request
+        # Implement SQL query to all orders
+        # request.orders = 10
         response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
 
         return response
