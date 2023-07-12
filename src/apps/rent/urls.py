34c9e2553d038d088c7320d7304e88156c500128
Pavel Rabekin <pavelrabekin@gmail.com>
2023-07-12 16:16:39 +0300

    added order view (console orders)

--- src/apps/rent/urls.py
+++ src/apps/rent/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from src.apps.rent.views import rent_list, console_list, rent_console
+from src.apps.rent.views import rent_list, rent_order_list, console_list, rent_console
 
 urlpatterns = [
     path("", rent_list, name="rent_list"),
+    path("orders/", rent_order_list, name="rent_order_list"),
     path("consoles/", console_list, name="console_list"),
     path("consoles/rent", rent_console, name="rent_console"),
 ]
--- src/apps/rent/views.py
+++ src/apps/rent/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, redirect
 from django.core.handlers.wsgi import WSGIRequest
+from django.db.models import Prefetch
 
+from src.apps.user.models import CustomUser
 from src.apps.rent.models import Console, ConsoleRent
 from src.apps.rent.forms import RentConsoleForm
 
 
 def rent_list(request: WSGIRequest):
-    return render(request, "rent_list.html")
+    return render(request, "rent/rent_list.html")
+
+
+def rent_order_list(request: WSGIRequest):
+    contex = {}
+    console_rent_qs = ConsoleRent.objects.select_related("console")
+    user = CustomUser.objects.prefetch_related(
+        Prefetch(
+            "rented_consoles", queryset=console_rent_qs, to_attr="console_orders"
+        )
+    ).filter(pk=request.user.id)[0]
+    contex["console_orders"] = user.console_orders
+    return render(request, "rent/orders/rent_order_list.html", contex)
 
 
 def console_list(request: WSGIRequest):
@@ -15,7 +29,7 @@ def console_list(request: WSGIRequest):
     form = RentConsoleForm()
     contex["consoles"] = consoles
     contex["form"] = form
-    return render(request, "console/console_list.html", contex)
+    return render(request, "rent/console/console_list.html", contex)
 
 
 def rent_console(request: WSGIRequest):
