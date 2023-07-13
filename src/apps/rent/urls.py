10e0ceb9375bb7037846d6da6776e1b3481f08c5
Pavel Rabekin <pavelrabekin@gmail.com>
2023-07-13 12:16:45 +0300

    updated menu for orders

--- src/apps/rent/urls.py
+++ src/apps/rent/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from src.apps.rent.views import rent_list, rent_order_list, console_list, rent_console
+from src.apps.rent.views import rent_list, console_order_list, console_list, rent_console, club_order_list, room_order_list
 
 urlpatterns = [
     path("", rent_list, name="rent_list"),
-    path("orders/", rent_order_list, name="rent_order_list"),
     path("consoles/", console_list, name="console_list"),
     path("consoles/rent", rent_console, name="rent_console"),
+    path("console_orders/", console_order_list, name="console_order_list"),
+    path("club_orders/", club_order_list, name="club_order_list"),
+    path("room_orders/", room_order_list, name="room_order_list"),
+
 ]
+
+
--- src/apps/rent/views.py
+++ src/apps/rent/views.py
@@ -3,7 +3,7 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.db.models import Prefetch
 
 from src.apps.user.models import CustomUser
-from src.apps.rent.models import Console, ConsoleRent
+from src.apps.rent.models import Console, ConsoleRent, Club, ClubRent, Room, RoomRent
 from src.apps.rent.forms import RentConsoleForm
 
 
@@ -11,7 +11,7 @@ def rent_list(request: WSGIRequest):
     return render(request, "rent/rent_list.html")
 
 
-def rent_order_list(request: WSGIRequest):
+def console_order_list(request: WSGIRequest):
     contex = {}
     console_rent_qs = ConsoleRent.objects.select_related("console")
     user = CustomUser.objects.prefetch_related(
@@ -20,7 +20,31 @@ def rent_order_list(request: WSGIRequest):
         )
     ).filter(pk=request.user.id)[0]
     contex["console_orders"] = user.console_orders
-    return render(request, "rent/orders/rent_order_list.html", contex)
+    return render(request, "rent/orders/consoles/console_order_list.html", contex)
+
+
+def club_order_list(request: WSGIRequest):
+    contex = {}
+    club_rent_qs = ClubRent.objects.select_related("club")
+    user = CustomUser.objects.prefetch_related(
+        Prefetch(
+            "rented_clubs", queryset=club_rent_qs, to_attr="club_orders"
+        )
+    ).filter(pk=request.user.id)[0]
+    contex["club_orders"] = user.club_orders
+    return render(request, "rent/orders/clubs/club_order_list.html", contex)
+
+
+def room_order_list(request: WSGIRequest):
+    contex = {}
+    club_rent_qs = RoomRent.objects.select_related("room")
+    user = CustomUser.objects.prefetch_related(
+        Prefetch(
+            "rented_rooms", queryset=club_rent_qs, to_attr="room_orders"
+        )
+    ).filter(pk=request.user.id)[0]
+    contex["room_orders"] = user.room_orders
+    return render(request, "rent/orders/rooms/room_order_list.html", contex)
 
 
 def console_list(request: WSGIRequest):
