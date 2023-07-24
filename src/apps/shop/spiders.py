a1bb5de5608a97c898564227e6e24a4b2ea6a557
Pavel Rabekin <pavelrabekin@gmail.com>
2023-07-24 16:25:54 +0300

    parser is developing

--- src/apps/shop/spiders.py
+++ src/apps/shop/spiders.py
@@ -22,19 +22,34 @@ class MouseSpider(scrapy.Spider):
         "playwright": True,
         "playwright_include_page": True,
         "playwright_page_methods": [
-            PageMethod("wait_for_selector", "a.schema-pagination__main")
+            # PageMethod("wait_for_selector", "a.schema-pagination__main")
+            PageMethod("wait_for_selector", ".schema-product__part.schema-product__part_1")
+        ],
+    }
+    meta2 = {
+        "playwright": True,
+        "playwright_include_page": True,
+        "playwright_page_methods": [
+            PageMethod("wait_for_selector", ".offers-description__link.offers-description__link_nodecor")
         ],
     }
 
     def start_requests(self):
         for url in self.start_urls:
-            yield scrapy.Request(url, self.parse, meta=self.meta)
+            yield scrapy.Request(url, self.get_urls, meta=self.meta)
 
-    async def parse(self, response: HtmlResponse, **kwargs) -> dict | None:
+    async def get_urls(self, response: HtmlResponse, **kwargs):
+        urls = []
         products = response.css(".schema-products .schema-product__group")[:10]
         for product in products:
-            href = product.css(".js-product-title-link").attrib.get("href")
+            url = product.css(".js-product-title-link").attrib.get("href")
+            urls.append(url)
             print()
-        # next_page = response.xpath('//*[@id="pagination-next"]').attrib.get("href")
-        # if next_page is not None:
-        #     yield response.follow(next_page, callback=self.parse)
+        for url in urls:
+            yield response.follow(url, callback=self.parse)
+            # yield response.follow(url, callback=self.parse, meta=self.meta2)
+
+    async def parse(self, response: HtmlResponse, **kwargs):
+        # price = response.css(".offers-description__price.offers-description__price_secondary")
+        price = response.css(".offers-description__price.offers-description__price_secondary").get()
+        print()
