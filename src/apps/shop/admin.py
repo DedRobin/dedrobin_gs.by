1bf5fcb34274e9b84bc333d6fb471d7da69ee81a
Pavel Rabekin <pavelrabekin@gmail.com>
2023-07-31 16:20:30 +0300

    added admin panel for 'Shop' app

--- src/apps/shop/admin.py
+++ src/apps/shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from src.apps.shop.models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    list_display = ("name", "product_type", "price", "description", "image", "updated_at")
+    fields = ("name", "product_type", "price", "description", "image", "updated_at")
+    list_filter = ("product_type",)
+    readonly_fields = ("updated_at",)
