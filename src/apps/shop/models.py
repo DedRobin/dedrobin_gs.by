78e7ebb49c2efc90db8a3b8de5320eab1db3498b
Pavel Rabekin <pavelrabekin@gmail.com>
2023-07-31 11:19:02 +0300

    added fields for "Mouse" model

--- src/apps/shop/models.py
+++ src/apps/shop/models.py
@@ -9,13 +9,14 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField()
     release_date = models.DateField()
+    image = models.URLField(blank=True, null=True)
 
 
 class Mouse(Product):
-    interface = models.CharField(max_length=100)
-    sensor_type = models.CharField(max_length=100)
-    max_sensor_resolution = models.CharField(max_length=100)
-    length_radius = models.CharField(max_length=100)
+    interface = models.CharField(max_length=100, blank=True, null=True)
+    sensor_type = models.CharField(max_length=100, blank=True, null=True)
+    max_sensor_resolution = models.CharField(max_length=100, blank=True, null=True)
+    length_radius = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"Mouse '{self.name}'"
