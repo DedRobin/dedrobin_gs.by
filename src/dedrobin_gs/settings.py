bd8df449898be23231ffbf3ee3970a5d52e6a559
Pavel Rabekin <pavelrabekin@gmail.com>
2023-08-09 13:55:22 +0300

    fixed css render

--- src/dedrobin_gs/settings.py
+++ src/dedrobin_gs/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "collectstatic"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, ""),
     os.path.join(BASE_DIR, "apps"),
 ]
 
