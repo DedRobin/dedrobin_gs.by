6b84d537497af2256f79483317c97aabf177d3b8
Pavel Rabekin <pavelrabekin@gmail.com>
2023-08-09 14:34:14 +0300

    now order quantity is displaying for each type of it

--- src/dedrobin_gs/middlewares.py
+++ src/dedrobin_gs/middlewares.py
@@ -14,9 +14,16 @@ class OrderCountMiddleware:
             r_room_count = Count("rented_rooms", distinct=True, filter=Q(rented_rooms__is_completed=False))
 
             result = CustomUser.objects.filter(pk=request.user.id).aggregate(
-                rented_thing_count=(r_console_count + r_club_count + r_room_count)
+                rented_console_count=r_console_count,
+                rented_club_count=r_club_count,
+                rented_room_count=r_room_count,
             )
-            request.rented_thing_count = result["rented_thing_count"]
+            request.rented_console_count = result["rented_console_count"]
+            request.rented_club_count = result["rented_club_count"]
+            request.rented_room_count = result["rented_room_count"]
+
+            request.sum_count = result["rented_console_count"] + result["rented_club_count"] + result[
+                "rented_room_count"]
 
         response = self.get_response(request)
         return response
